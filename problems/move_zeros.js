b65f4c71de491fbe21d4590ad789722c2571fa10
abdelrazekAli <dev.abdelrazek@gmail.com>
2023-08-20 08:46:07 +0300

    Add problems

---
+++ problems/move_zeros.js
@@ -0,0 +1,28 @@
+/*
+    - Given an integer array nums, move all 0's to the end of it
+     while maintaining the relative order of non-zero elements.
+
+    - Note that you must do this in-place without making
+      a copy of the array.
+
+    - Example 1:
+        Input: nums = [0,1,0,3,12]
+        Output: [1,3,12,0,0]
+*/
+
+const move_zeros = (nums) => {
+    let lastNonZeroIdx = 0
+
+    // Move non zero elements to the top of array
+    for (i = 0; i < nums.length; i++) {
+        if (nums[i] != 0) {
+            nums[lastNonZeroIdx++] = nums[i]
+        }
+    }
+
+    // Make elements equal to zero from the last non zero element 
+    for (i = lastNonZeroIdx; i < nums.length; i++) {
+        nums[i] = 0
+    }
+    return nums
+}
